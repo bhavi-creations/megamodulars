1479c5d70549229ffe33bac09ac471f6aef51a5b
manimalladi <manimalladi05@gmail.com>
2026-08-14 12:22:47 +0530

    faq sectin

--- header.php
+++ header.php
@@ -6,11 +6,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mega Modular Industries - Navbar</title>
-    <link rel="stylesheet" href="./assets/style.css">
     <!-- Bootstrap 5.3 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="./assets/style.css">
 
 
  
@@ -51,7 +51,7 @@
 
                     <div class="offcanvas-body">
                         <!-- Navigation Links -->
-                        <ul class="navbar-nav mx-auto align-items-lg-center">
+                        <ul class="navbar-nav mobile-nav mx-auto align-items-lg-center">
                             <li class="nav-item">
                                 <a class="nav-link<?= $currentPage === 'index.php' ? ' active' : '' ?>" href="index.php">Home</a>
                             </li>
